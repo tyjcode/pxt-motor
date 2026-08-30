589f70c2c19ccc9df93c9d7982a7832c36b985f5
tyjcode <121700458+tyjcode@users.noreply.github.com>
2026-08-30 14:18:37 +0900

    Update frequency settings for PCA9685 initialization

--- main.ts
+++ main.ts
@@ -137,9 +137,9 @@ namespace motor {
     function initPCA9685(): void {
         i2cWrite(PCA9685_ADDRESS, MODE1, 0x00)
         // setFreq(50);
-		//setFreq(1000); //for test 1kHz
 		//setFreq(100); //for test 100Hz
-		setFreq(500); //for test 500Hz
+		//setFreq(500); //for test 500Hz
+		setFreq(1000); //for test 1kHz
 		
         initialized = true
     }
